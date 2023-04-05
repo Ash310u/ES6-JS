e2c055215f9444b5a5c5bda4af342a5e42484f69
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-05 09:57:20 +0530

    Commited Promise & fetch API  syntax

--- promiseFetch.js
+++ promiseFetch.js
@@ -1,12 +1,18 @@
-Promise = new Promise((myresolve, myreject) => {
-    setTimeout(() => {
-        // myresolve();
-        myreject();
-    }, 4000);
-});
-Promise
-    // these "then" are callback functions
-    .then(() => console.log('because the promise is "resolve" after that "then" works'))
-    .then(() => console.log('we can use multiple "then"'))
-    .catch(() => console.log('because the promise is "reject" after that "catch" works'))
+// Promise = new Promise((myresolve, myreject) => {
+//     setTimeout(() => {
+//         // myresolve();
+//         myreject();
+//     }, 4000);
+// });
+// Promise
+//     // these "then" are callback functions
+//     .then(() => console.log('because the promise is "resolve" after that "then" works'))
+//     .then(() => console.log('we can use multiple "then"'))
+//     .catch(() => console.log('because the promise is "reject" after that "catch" works'));
 
+
+let url = 'https://jsonplaceholder.typicode.com/posts/'
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
