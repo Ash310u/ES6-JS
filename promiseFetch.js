e8f654370aad17c07202ad7698739f94bc0764c9
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-06 13:57:45 +0530

    Commited fetch.js

--- promiseFetch.js
+++ promiseFetch.js
@@ -15,4 +15,5 @@ let url = 'https://jsonplaceholder.typicode.com/posts/'
 
 fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data));
+    // .then(data => console.log(data));
+    .catch(error => console.log("bad ass",error));
