2c753b2437203bf06625cc8205649f66d8c95a1c
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-10 15:28:04 +0530

    Commited: made a comment for all the test cases

--- generators.js
+++ generators.js
@@ -160,20 +160,23 @@ const children = [
     new comments('meh',[])
 ]
 
-const tree = [
-    new comments('great post',children),
-    new comments('wow cool', children)
-]
-    
-const headTree = new comments('Head tree', tree)
+const tree = new comments('great post',children)
+
+// const tree = [
+//     new comments('great post',children),
+//     new comments('wow cool', children)
+// ]
+// const headTree = new comments('Head tree', tree)
 
 // we can seen the content of "great post" and then an "array" of "children"
-console.log(headTree)
-console.log(headTree.children[1])
+console.log(tree)
 
-// const values = [];
+// console.log(headTree)
+// console.log(headTree.children[1])
 
-// for (let value of tree) {
-//     values.push(value);
-// }
-// console.log(values);
+const values = [];
+
+for (let value of tree) {
+    values.push(value);
+}
+console.log(values);
